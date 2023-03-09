abde6d90e60f4fb8a951a539bf64c52c7d1d0cdc
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2023-03-09 09:14:11 -0300

    Add files via upload

---
+++ ParseTryParse/ParseTryParse/Program.cs
@@ -0,0 +1,18 @@
+﻿var valor1 = int.Parse("123");
+var valor2 = bool.Parse("true");
+var valor3 = DateTime.Parse("01/01/2001");
+
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+
+var deuCerto = int.TryParse("8910", out var valor);
+Console.WriteLine("\b");
+
+Console.WriteLine("TryParse");
+Console.WriteLine(deuCerto);
+Console.WriteLine(valor);
+
+
+Console.ReadLine();
